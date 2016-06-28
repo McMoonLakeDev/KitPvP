0fde6b90c9fa6521f58c9111311a8cea5b34f809
McMoonLake <ablu2g@sina.com>
2016-06-28 15:20:15 +0800

    init kit pvp plugin

---
+++ src/com/minecraft/moonlake/kitpvp/KitPvPPlugin.java
@@ -0,0 +1,9 @@
+package com.minecraft.moonlake.kitpvp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by MoonLake on 2016/6/28.
+ */
+public class KitPvPPlugin extends JavaPlugin {
+}
